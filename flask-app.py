52377463440f35b9b1a95798612a07acfb143184
NyoNyoHtwe <lovelynyo31@gmail.com>
2018-06-13 11:28:20 -0800

    commit file

--- flask-app.py
+++ flask-app.py
@@ -5,6 +5,12 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+     <html>
+     <head><title>This is my flask app</title></head>
+     <body><h1>Hello!</h1>
+     <p>Good enening</p>
+     </body></html>
+
 if __name__ == "__main__":
     
   app.run(debug=True)
